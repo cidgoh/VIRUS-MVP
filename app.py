13cd4d5fa645622136198e4070333f2256ec701a
Ivan S Gill <isgill93@student.ubc.ca>
2021-03-07 14:02:10 -0800

    Modularize div generation

--- app.py
+++ app.py
@@ -2,67 +2,19 @@
 
 import dash
 import dash_bootstrap_components as dbc
-import dash_core_components as dcc
-import dash_html_components as html
 from dash.dependencies import Input, Output
 
 from data_parser import get_data
-import heatmap_generator
+import div_generator
 import table_generator
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 data = get_data()
 
-heatmap_row = html.Div([
-    dbc.Row([
-        dbc.Col(
-            html.Div(
-                dcc.Graph(
-                    id="heatmap-left-fig",
-                    figure=heatmap_generator.get_heatmap_left_fig(data)
-                ),
-                style={"width": "90vw", "overflowX": "hidden"}
-            ),
-            width=1
-        ),
-        dbc.Col(
-            html.Div(
-                dcc.Graph(
-                    id="heatmap-center-fig",
-                    figure=heatmap_generator.get_heatmap_center_fig(data)
-                ),
-                style={"overflowX": "scroll"}
-            ),
-            width=10
-        ),
-        dbc.Col(
-            html.Div(
-                dcc.Graph(
-                    id="heatmap-right-fig",
-                    figure=heatmap_generator.get_heatmap_right_fig(data)
-                ),
-            ),
-            width=1
-        ),
-    ], no_gutters=True),
-])
-
-table_row = html.Div([
-    dbc.Row(
-        dbc.Col(
-            dcc.Graph(
-                id="table",
-                figure=
-                table_generator.get_table_fig(data,data["heatmap_y"][0])
-            )
-        ),
-    )
-])
-
 app.layout = dbc.Container([
-    heatmap_row,
-    table_row
+    div_generator.get_heatmap_row_div(data),
+    div_generator.get_table_row_div(data)
 ], fluid=True)
 
 @app.callback(
