2c4d63867ae3a46ba4280b4ed0c06156ddecde99
ivansg44 <isgill93@student.ubc.ca>
2021-03-17 10:09:37 -0700

    Integrate 20210314 data (#13)
    * And new clade defining mutations format
    
    * Required some changes to clade_defining_mutations parsing
    
    * Some cleanup was required for new P.1 data file

--- clade_defining_mutations_generator.py
+++ clade_defining_mutations_generator.py
@@ -7,13 +7,31 @@ import os
 def get_clade_defining_mutations():
     """"TODO..."""
     ret = {}
-    with open("VOC clade-defining mutations - Catalog.tsv") as fp:
-        reader = csv.DictReader(fp, delimiter="\t")
+    with open("VOC clade-defining mutations - gff3.tsv") as fp:
+        fieldnames = [
+            "#seqid", "#source", "#type", "#start", "#end", "#score",
+            "#strand", "#phase", "#attributes"
+        ]
+        reader = csv.DictReader(fp, delimiter="\t", fieldnames=fieldnames)
+        for _ in range(0, 5):
+            next(reader)
         for row in reader:
-            strain = row["lineage"]
+            attributes =\
+                get_clade_defining_mutation_attributes(row["#attributes"])
+            strain = attributes["ID"].split("-")[0]
             if strain not in ret:
                 ret[strain] = set()
-            ret[strain].add(row["nucleotide_position"])
+            ret[strain].add(row["#start"])
+    return ret
+
+
+def get_clade_defining_mutation_attributes(gff3_attributes_str):
+    gff3_single_attribute_str_list = gff3_attributes_str.split(";")
+    ret = {}
+    for gff3_single_attribute_str in gff3_single_attribute_str_list:
+        if "=" in gff3_single_attribute_str:
+            [key, val] = gff3_single_attribute_str.split("=")
+            ret[key] = val
     return ret
 
 
@@ -23,7 +41,6 @@ def main():
     with os.scandir("clade_defining_mutations_data") as it:
         for entry in it:
             strain = entry.name.split("_")[0]
-            fieldnames = []
             filtered_rows = []
             with open(entry.path, "r") as fp:
                 reader = csv.DictReader(fp, delimiter="\t")
