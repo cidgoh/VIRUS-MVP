4c1c1843008dd9d925d0a8f421dfc124d6aa7421
Ivan S Gill <isgill93@student.ubc.ca>
2021-04-22 13:16:35 -0700

    wip incode doc

--- table_generator.py
+++ table_generator.py
@@ -1,10 +1,18 @@
-"""TODO..."""
+"""Functions that generate figure in table view."""
 
 import plotly.graph_objects as go
 
 
 def get_table_fig(data, strain):
-    """TODO..."""
+    """Get Plotly figure representing table view.
+
+    :param data: ``data_parser.get_data`` return value
+    :type data: dict
+    :param strain: Strain to show table for
+    :type strain: str
+    :return: Plotly figure
+    :rtype: go.Figure
+    """
     table_obj = get_table_obj(data, strain)
     ret = go.Figure(table_obj)
     ret.update_layout(title={
@@ -15,7 +23,15 @@ def get_table_fig(data, strain):
 
 
 def get_table_obj(data, strain):
-    """TODO..."""
+    """Get Plotly graph object representing table view.
+
+    :param data: ``data_parser.get_data`` return value
+    :type data: dict
+    :param strain: Strain to show table for
+    :type strain: str
+    :return: Plotly graph object
+    :rtype: go.Figure
+    """
     # TODO these should be determined automatically based on data
     header_vals = [
         "pos", "mutation_name", "ref", "alt", "alt_freq", "functions"
