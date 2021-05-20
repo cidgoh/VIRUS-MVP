2e4c91599664188744417e31ef32fd674f44d0f8
Ivan S Gill <isgill93@student.ubc.ca>
2021-05-20 14:17:48 -0700

    Accurate gene bar with colors

--- histogram_generator.py
+++ histogram_generator.py
@@ -1,5 +1,7 @@
 """TODO"""
 
+import json
+
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import plotly.graph_objects as go
@@ -29,7 +31,7 @@ def get_histogram_fig(data):
                         row_heights=[0.7, 0.3],
                         vertical_spacing=0)
     ret.add_trace(get_histogram_main_obj(data), row=1, col=1)
-    for bar_obj in get_histogram_gene_bar_obj(data):
+    for bar_obj in get_histogram_gene_bar_obj_list():
         ret.add_trace(bar_obj, row=2, col=1)
     ret.update_layout(
         margin={"t": 0, "b": 0, "l": 0, "r": 0, "pad": 0},
@@ -57,28 +59,28 @@ def get_histogram_main_obj(data):
     return ret
 
 
-def get_histogram_gene_bar_obj(data):
+def get_histogram_gene_bar_obj_list():
     """TODO"""
-    ret = [
-        go.Bar(name='SF Zoo',
-               x=[1000],
-               y=["foo"],
-               orientation="h",
-               text=["ham"],
-               textposition="inside",
-               insidetextanchor="middle",
-               insidetextfont={"color": "white"},
-               marker={"color": "red"},
-               showlegend=False),
-        go.Bar(name='LA Zoo',
-               x=[28903],
-               y=["foo"],
-               orientation="h",
-               text=["spam"],
-               textposition="inside",
-               insidetextanchor="middle",
-               insidetextfont={"color": "white"},
-               marker={"color": "blue"},
-               showlegend=False),
-    ]
+    with open("gene_positions.json") as fp:
+        gene_positions_dict = json.load(fp)
+    with open("gene_colors.json") as fp:
+        gene_colors_dict = json.load(fp)
+
+    ret = []
+    total_bar_len_so_far = 0
+    for gene in gene_positions_dict:
+        this_bar_len = gene_positions_dict[gene]["end"] - total_bar_len_so_far
+        total_bar_len_so_far += this_bar_len
+        bar_obj = go.Bar(name=gene,
+                         x=[this_bar_len],
+                         y=["foo"],
+                         orientation="h",
+                         text=[gene],
+                         textposition="inside",
+                         insidetextanchor="middle",
+                         insidetextfont={"color": "white"},
+                         marker={"color": gene_colors_dict[gene]},
+                         showlegend=False)
+        ret.append(bar_obj)
+
     return ret
