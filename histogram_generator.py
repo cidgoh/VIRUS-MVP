0a384284901f3ab6ebc3c3464e8ee5d908b77053
Ivan S Gill <isgill93@student.ubc.ca>
2021-05-20 13:38:58 -0700

    Add stub gene bar

--- histogram_generator.py
+++ histogram_generator.py
@@ -3,6 +3,7 @@
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 
 def get_histogram_row_div(data):
@@ -23,24 +24,61 @@ def get_histogram_row_div(data):
 
 def get_histogram_fig(data):
     """TODO"""
-    ret = go.Figure(
-        get_histogram_obj(data),
-        layout=go.Layout(
-            margin={"t": 0, "b": 0, "l": 0, "r": 0, "pad": 0},
-            plot_bgcolor="white",
-            bargap=0.1,
-            font={"size": 18},
-            yaxis={"visible": False}
-        )
+    ret = make_subplots(rows=2,
+                        cols=1,
+                        row_heights=[0.7, 0.3],
+                        vertical_spacing=0)
+    ret.add_trace(get_histogram_main_obj(data), row=1, col=1)
+    for bar_obj in get_histogram_gene_bar_obj(data):
+        ret.add_trace(bar_obj, row=2, col=1)
+    ret.update_layout(
+        margin={"t": 0, "b": 0, "l": 0, "r": 0, "pad": 0},
+        plot_bgcolor="white",
+        bargap=0.1,
+        font={"size": 18},
+        xaxis1={"visible": False, "range": [1, 29903]},
+        xaxis2={"visible": False, "range": [1, 29903]},
+        yaxis1={"visible": False},
+        yaxis2={"visible": False},
+        barmode="stack"
     )
+
     return ret
 
 
-def get_histogram_obj(data):
+def get_histogram_main_obj(data):
     """TODO"""
     ret = go.Histogram(
         x=[int(x) for x in data["histogram_x"]],
         xbins={"start": 1, "size": 100},
         marker={"color": "black"},
+        showlegend=False
     )
+    return ret
+
+
+def get_histogram_gene_bar_obj(data):
+    """TODO"""
+    ret = [
+        go.Bar(name='SF Zoo',
+               x=[1000],
+               y=["foo"],
+               orientation="h",
+               text=["ham"],
+               textposition="inside",
+               insidetextanchor="middle",
+               insidetextfont={"color": "white"},
+               marker={"color": "red"},
+               showlegend=False),
+        go.Bar(name='LA Zoo',
+               x=[28903],
+               y=["foo"],
+               orientation="h",
+               text=["spam"],
+               textposition="inside",
+               insidetextanchor="middle",
+               insidetextfont={"color": "white"},
+               marker={"color": "blue"},
+               showlegend=False),
+    ]
     return ret
