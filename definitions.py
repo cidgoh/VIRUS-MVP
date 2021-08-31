3e62ce00efe9badbcfc36971e82208f6686e529a
ivansg44 <isgill93@student.ubc.ca>
2021-08-31 15:06:42 -0700

    Remove hardcoded path when uploading file (#62)
    * And specify assets directory path as global variable
    
    * And rename dash app from foo to covid-mvp

--- definitions.py
+++ definitions.py
@@ -4,8 +4,9 @@ import os
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 REFERENCE_DATA_DIR = os.path.join(ROOT_DIR, "reference_data")
 USER_DATA_DIR = os.path.join(ROOT_DIR, "user_data")
-GENE_COLORS_PATH = os.path.join(ROOT_DIR, "assets/gene_colors.json")
-GENE_POSITIONS_PATH = os.path.join(ROOT_DIR, "assets/gene_positions.json")
+ASSETS_DIR = os.path.join(ROOT_DIR, "assets")
+GENE_COLORS_PATH = os.path.join(ROOT_DIR, "assets", "gene_colors.json")
+GENE_POSITIONS_PATH = os.path.join(ROOT_DIR, "assets", "gene_positions.json")
 
 with open(GENE_COLORS_PATH) as fp:
     GENE_COLORS_DICT = json.load(fp)
