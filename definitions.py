60bbd94575313ef7b0cdefb18b2d999dc29c6fa6
Ivan S Gill <ivan_gill@sfu.ca>
2023-07-07 11:48:11 -0700

    Updated definitions

--- definitions.py
+++ definitions.py
@@ -1,3 +1,4 @@
+from csv import DictReader
 import json
 import os
 
@@ -6,9 +7,13 @@ REFERENCE_DATA_DIR = os.path.join(ROOT_DIR, "reference_data")
 USER_DATA_DIR = os.path.join(ROOT_DIR, "user_data")
 ASSETS_DIR = os.path.join(ROOT_DIR, "assets")
 NF_NCOV_VOC_DIR = os.path.join(ROOT_DIR, "nf-ncov-voc")
-GENE_COLORS_PATH = os.path.join(ROOT_DIR, "assets", "gene_colors.json")
-GENE_POSITIONS_PATH = os.path.join(ROOT_DIR, "assets", "gene_positions.json")
-NSP_POSITIONS_PATH = os.path.join(ROOT_DIR, "assets", "nsp_positions.json")
+GENE_COLORS_PATH = os.path.join(ASSETS_DIR, "gene_colors.json")
+GENE_POSITIONS_PATH = os.path.join(ASSETS_DIR, "gene_positions.json")
+NSP_POSITIONS_PATH = os.path.join(ASSETS_DIR, "nsp_positions.json")
+DEFAULT_REFERENCE_HIDDEN_STRAINS_PATH = \
+    os.path.join(ASSETS_DIR, "default_reference_hidden_strains.json")
+DEFAULT_REFERENCE_STRAIN_ORDER_PATH = \
+    os.path.join(ASSETS_DIR, "default_reference_strain_order.json")
 REFERENCE_SURVEILLANCE_REPORTS_DIR = \
     os.path.join(ROOT_DIR, "reference_surveillance_reports")
 USER_SURVEILLANCE_REPORTS_DIR = \
@@ -22,3 +27,9 @@ with open(GENE_POSITIONS_PATH) as fp:
 
 with open(NSP_POSITIONS_PATH) as fp:
     NSP_POSITIONS_DICT = json.load(fp)
+
+with open(DEFAULT_REFERENCE_HIDDEN_STRAINS_PATH) as fp:
+    DEFAULT_REFERENCE_HIDDEN_STRAINS = json.load(fp)
+
+with open(DEFAULT_REFERENCE_STRAIN_ORDER_PATH) as fp:
+    DEFAULT_REFERENCE_STRAIN_ORDER = json.load(fp)
