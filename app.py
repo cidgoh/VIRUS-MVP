bd81405f0d4e84f9e3d9931881c335bd730b34a0
ivansg44 <ivan_gill@sfu.ca>
2023-09-07 11:20:15 -0700

    Scroll heatmap to specified gene on page launch (#178)

--- app.py
+++ app.py
@@ -188,6 +188,8 @@ def launch_app(_):
         dcc.Store(id="strain-to-del"),
         dcc.Store(id="deleted-strain"),
         dcc.Store(id="positions-jumped-to"),
+        # TODO starting gene should be part of a config file
+        dcc.Store(id="default-starting-gene", data="S"),
         # Used to update certain figures only when necessary
         dcc.Store(id="heatmap-x-len", data=len(data_["heatmap_x_nt_pos"])),
         dcc.Store(id="heatmap-y-strains",
@@ -1412,6 +1414,16 @@ app.clientside_callback(
     ),
     Output("make-histogram-rel-pos-bar-dynamic", "data"),
     Input("heatmap-nt-pos-axis-fig", "figure"),
+    State("data", "data")
+)
+app.clientside_callback(
+    ClientsideFunction(
+        namespace="clientside",
+        function_name="jumpToDefaultStartingGenePos"
+    ),
+    Output("default-starting-gene", "data"),
+    Input("heatmap-gene-bar-fig", "figure"),
+    State("default-starting-gene", "data"),
     State("data", "data")
 )
 app.clientside_callback(
