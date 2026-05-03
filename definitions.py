01cb818de87da9052f017a031cba79eaf72082f3
Ivan S Gill <ivan_gill@sfu.ca>
2026-05-03 13:44:48 -0700

    Remove stale vars from definitions

--- definitions.py
+++ definitions.py
@@ -11,7 +11,6 @@ ASSETS_DIR = os.path.join(ROOT_DIR, "assets")
 NF_NCOV_VOC_DIR = os.path.join(ROOT_DIR, "nf-ncov-voc")
 VIRUS_REFERENCE_SEGMENT_PATH = \
     os.path.join(ASSETS_DIR, "virus_reference_segment.json")
-GENOME_CONFIG_PATH = os.path.join(ASSETS_DIR, "genome_config.json")
 RUN_INFO_PATH = os.path.join(ASSETS_DIR, "run_info.json")
 DEFAULT_REFERENCE_HIDDEN_STRAINS_PATH = \
     os.path.join(ASSETS_DIR, "default_reference_hidden_strains.json")
@@ -25,41 +24,6 @@ README_PATH = os.path.join(ROOT_DIR, "README.md")
 
 with open(VIRUS_REFERENCE_SEGMENT_PATH) as fp:
     VIRUS_REFERENCE_SEGMENT_DICT = json.load(fp)
-
-with open(GENOME_CONFIG_PATH) as fp:
-    GENOME_CONFIG_DICT = json.load(fp)
-
-GENOME_LEN = GENOME_CONFIG_DICT["Src"]["end"]
-
-gene_bar_components = \
-    [e for e in GENOME_CONFIG_DICT if GENOME_CONFIG_DICT[e]["type"]
-     in ["CDS", "five_prime_UTR", "three_prime_UTR", "INTERGENIC"]]
-GENE_COLORS_DICT = \
-    {k: GENOME_CONFIG_DICT[k]["color"] for k in gene_bar_components}
-GENE_POSITIONS_DICT = \
-    {k: {x: GENOME_CONFIG_DICT[k][x] for x in ["start", "end"]}
-     for k in gene_bar_components[:-1]}
-
-first_component = min(GENE_POSITIONS_DICT,
-                   key=lambda k: GENE_POSITIONS_DICT[k]["start"])
-if GENE_POSITIONS_DICT[first_component]["start"] == 1:
-    FIRST_REGION = [1, GENE_POSITIONS_DICT[first_component]["end"]]
-else:
-    FIRST_REGION = [1, GENE_POSITIONS_DICT[first_component]["start"]-1]
-
-last_component = max(GENE_POSITIONS_DICT,
-                  key=lambda k: GENE_POSITIONS_DICT[k]["end"])
-if GENE_POSITIONS_DICT[last_component]["end"] == GENOME_LEN:
-    LAST_REGION = [GENE_POSITIONS_DICT[last_component]["start"], GENOME_LEN]
-else:
-    LAST_REGION = [GENE_POSITIONS_DICT[last_component]["end"]+1, GENOME_LEN]
-
-nsp_bar_components = \
-    [e for e in GENOME_CONFIG_DICT
-     if GENOME_CONFIG_DICT[e]["type"] == "mature_protein_region_of_CDS"]
-NSP_POSITIONS_DICT = \
-    {k: {x: GENOME_CONFIG_DICT[k][x] for x in ["start", "end"]}
-     for k in nsp_bar_components}
 
 with open(RUN_INFO_PATH) as fp:
     RUN_INFO_DICT = json.load(fp)
